7ea2f325d5272876e7e207aef8930377a8ef2107
StephenV <crackerfracks@gmail.com>
2025-05-21 23:32:16 -0400

    trying to get lua-ls configured right

--- lua/custom/plugins/init(home-dotconfig-nvim-lua-custom-plugins).lua
+++ lua/custom/plugins/init(home-dotconfig-nvim-lua-custom-plugins).lua
@@ -154,6 +154,17 @@ return {
     end,
   },
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
     -- dependencies = 'rafamadriz/friendly-snippets',
@@ -211,8 +222,18 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
+
+      -- Adding in the providers finally lol
 
       -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
       -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
@@ -565,6 +586,7 @@ return {
   },
   {
     "mrjones2014/smart-splits.nvim",
+    lazy = false,
     config = function()
       require('smart-splits').setup({
         -- Ignored buffer types (only while resizing)
