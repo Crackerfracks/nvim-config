3250a9757953c802847d69cdae5cf7bb9d675a9a
Crackerfracks <130577651+Crackerfracks@users.noreply.github.com>
2025-11-10 21:57:07 -0500

    JFC Part 5 the revertening

--- nvim/lua/custom/plugins/flash.lua
+++ nvim/lua/custom/plugins/flash.lua
@@ -32,24 +32,24 @@ return {
         shade = 4,
       },
     },
-    remote_op = {
-      restore = true,
-      motion = true,
-    },
-    modes = {
-      remote = {
-        jump = {
-          autojump = false,
-        },
-        label = {
-          autojump = false,
-        },
-        remote_op = {
-          restore = true,
-          motion = true,
-        },
-      },
-    },
+    -- remote_op = {
+    --   restore = true,
+    --   motion = true,
+    -- },
+    -- modes = {
+    --   remote = {
+    --     jump = {
+    --       autojump = false,
+    --     },
+    --     label = {
+    --       autojump = false,
+    --     },
+    --     remote_op = {
+    --       restore = true,
+    --       motion = true,
+    --     },
+    --   },
+    -- },
   },
   -- stylua: ignore
   keys = {
@@ -81,57 +81,57 @@ return {
       desc = "Flash Treesitter"
     },
 
-    {
-      "<tab>",
-      mode = {
-        "n",
-      },
-      function() -- <-- Why are some functions defined as variables, some are given names as an actual function, and still others like this are passed raw?
-        local Flash = require("flash")
-
-        ---@param opts Flash.Format
-        local function format(opts)
-          -- always show first and second label
-          return {
-            { opts.match.label1, "FlashMatch" },
-            { opts.match.label2, "FlashLabel" },
-          }
-        end
-
-        Flash.jump({
-          search = { mode = "search" },
-          label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
-          pattern = [[\<]],
-          action = function(match, state)
-            state:hide()
-            Flash.jump({
-              search = { max_length = 0 },
-              highlight = { matches = false },
-              label = { format = format },
-              matcher = function(win)
-                -- limit matches to the current label
-                return vim.tbl_filter(function(m)
-                  return m.label == match.label and m.win == win
-                end, state.results)
-              end,
-              labeler = function(matches)
-                for _, m in ipairs(matches) do
-                  m.label = m.label2 -- use the second label
-                end
-              end,
-            })
-          end,
-          labeler = function(matches, state)
-            local labels = state:labels()
-            for m, match in ipairs(matches) do
-              match.label1 = labels[math.floor((m - 1) / #labels) + 1]
-              match.label2 = labels[(m - 1) % #labels + 1]
-              match.label = match.label1
-            end
-          end,
-        })
-      end
-    },
+    -- {
+    --   "<tab>",
+    --   mode = {
+    --     "n",
+    --   },
+    --   function() -- <-- Why are some functions defined as variables, some are given names as an actual function, and still others like this are passed raw?
+    --     local Flash = require("flash")
+    --
+    --     ---@param opts Flash.Format
+    --     local function format(opts)
+    --       -- always show first and second label
+    --       return {
+    --         { opts.match.label1, "FlashMatch" },
+    --         { opts.match.label2, "FlashLabel" },
+    --       }
+    --     end
+    --
+    --     Flash.jump({
+    --       search = { mode = "search" },
+    --       label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+    --       pattern = [[\<]],
+    --       action = function(match, state)
+    --         state:hide()
+    --         Flash.jump({
+    --           search = { max_length = 0 },
+    --           highlight = { matches = false },
+    --           label = { format = format },
+    --           matcher = function(win)
+    --             -- limit matches to the current label
+    --             return vim.tbl_filter(function(m)
+    --               return m.label == match.label and m.win == win
+    --             end, state.results)
+    --           end,
+    --           labeler = function(matches)
+    --             for _, m in ipairs(matches) do
+    --               m.label = m.label2 -- use the second label
+    --             end
+    --           end,
+    --         })
+    --       end,
+    --       labeler = function(matches, state)
+    --         local labels = state:labels()
+    --         for m, match in ipairs(matches) do
+    --           match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+    --           match.label2 = labels[(m - 1) % #labels + 1]
+    --           match.label = match.label1
+    --         end
+    --       end,
+    --     })
+    --   end
+    -- },
 
     ----------------------------------------------------------------------------
     -- character motions (f/F/t/T) — Flash “char” mode
